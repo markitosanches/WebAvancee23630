ff2de82bc9260bbd4f53b539c778a01b993df63a
Marcos <markitosanches@gmail.com>
2024-12-03 16:06:40 -0500

    Seance 16

---
+++ seance16_mvc/config.php
@@ -0,0 +1,5 @@
+<?php
+define('BASE', '/webAvancee23620/seance16_mvc');
+define('ASSET', '/webAvancee23620/seance16_mvc/public/');
+
+?>
---
+++ seance16_mvc/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+use App\Routes\Route;
+
+use App\Controllers\HomeController;
+use App\Controllers\ClientController;
+
+Route::get('/home', 'HomeController@index');
+Route::get('/about', 'HomeController@about');
+
+Route::get('/clients', 'ClientController@index');
+Route::get('/client/show', 'ClientController@show');
+Route::get('/client/create', 'ClientController@create');
+Route::post('/client/create', 'ClientController@store');
+Route::get('/client/edit', 'ClientController@edit');
+Route::post('/client/edit', 'ClientController@update');
+Route::post('/client/delete', 'ClientController@delete');
+
+Route::dispatch();
+
+?>
